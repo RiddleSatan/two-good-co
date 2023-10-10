dbc6978b4b50294bc0aedbca4d84d1433a221ed4
RiddleSatan <109842309+RiddleSatan@users.noreply.github.com>
2023-10-10 21:21:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,144 @@
+function scrollanimate() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector(".main"),
+    smooth: true,
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  // tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy(".main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector(".main").style.transform
+      ? "transform"
+      : "fixed",
+  });
+
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+}
+
+function vidcon() {
+  let videocon = document.querySelector(".video-container");
+  let cir = document.querySelector(".circle");
+
+  videocon.addEventListener("mouseenter", function () {
+    gsap.to(cir, {
+      scale: 1,
+      opacity: 1,
+    });
+  });
+  videocon.addEventListener("mouseleave", function () {
+    gsap.to(cir, {
+      scale: 0,
+      opacity: 0,
+    });
+  });
+
+  videocon.addEventListener("mousemove", function (dets) {
+    gsap.to(cir, {
+      left: dets.clientX - 50,
+      top: dets.clientY - 50,
+    });
+  });
+}
+
+function loadinganime() {
+  gsap.from(".home h1", {
+    y: 100,
+    opacity: 0,
+    delay: 0.3,
+    duration: 0.3,
+    stagger: 0.2,
+  });
+  gsap.from(".home .video-container", {
+    scale: 0.9,
+    opacity: 0,
+    delay: 0.9,
+    duration: 0.5,
+    stagger: 0.2,
+  });
+}
+
+function mousemove() {
+  document.addEventListener("mousemove", function (dets) {
+    gsap.to(".cursor", {
+      left: dets.clientX,
+      top: dets.clientY,
+    });
+  });
+
+  document.querySelectorAll(".child").forEach(function (i) {
+    i.addEventListener("mouseenter", function () {
+      gsap.to(".cursor", {
+        transform: `translate(-50%,-50%) scale(1)`,
+      });
+    });
+  });
+
+  document.querySelectorAll(".child").forEach(function (i) {
+    i.addEventListener("mouseleave", function () {
+      gsap.to(".cursor", {
+        transform: `translate(-50%,-50%) scale(0)`,
+      });
+    });
+  });
+}
+
+
+
+function navanime(){
+  window.onload = function () {
+    gsap.to(".navleft svg", {
+      transform: `translateY(-100%)`,
+      scrollTrigger: {
+        trigger: ".home",
+        scroller: ".main",
+        
+        start: "top 0",
+        end: "top -5%",
+        scrub: 0.8,
+      },
+    });
+  
+    gsap.to(".navlink .linka ", {
+      transform: `translateY(-100%)`,
+      opacity: 0,
+      scrollTrigger: {
+        trigger: ".home",
+        scroller: ".main",
+        
+        start: "top 0",
+        end: "top -5%",
+        scrub: 0.8,
+      },
+    });
+  };
+}
+
+
+navanime();
+scrollanimate();
+mousemove();
+vidcon();
+loadinganime();
